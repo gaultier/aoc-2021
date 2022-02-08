708fa29b94056740db08e822d8d7b2e18173ce50
Philippe Gaultier <philippe.gaultier@ppro.com>
2022-02-08 10:09:58 +0100

    extract count-increase

--- 1-a.lisp
+++ 1-a.lisp
@@ -14,9 +14,13 @@
         (loop for line = (read-line in nil)
             while line do (let ((n (parse-integer line)))
                             (if last-num 
-                              (if (> n last-num)
-                                (incf increase-count)))
+                                (incf increase-count (count-increase n last-num)))
                             (setf last-num n))))
     increase-count))
+
+(defun count-increase (n acc)
+  (if (> n acc)
+      1
+      0))
 
 (print (solve "1a.txt"))
